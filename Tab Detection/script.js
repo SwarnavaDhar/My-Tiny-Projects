5f0a17a1f01702fcc60032dc7128d28e2c881785
swarnavadhar2000@gmail.com <swarnavadhar2000@gmail.com>
2024-06-19 02:05:11 +0530

    Tab Detection

---
+++ Tab Detection/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('blur', function() {
+    document.title = "Another tab";
+});
+    window.addEventListener('focus', function() {
+    document.title = "On this tab";
+});
+
+
+
+
